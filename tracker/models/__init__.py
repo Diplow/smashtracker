c0efeb5bd126a42ecc82c31e19fd59f7e6286899
Diplo <u.boillot@gmail.com>
2016-06-11 11:42:30 +0200

    fix __init__.py models

--- tracker/models/__init__.py
+++ tracker/models/__init__.py
@@ -1,9 +1,9 @@
-from tracker.models.user import Job, User
+from tracker.models.user import Group, User
 from tracker.models.smash import Character, Map, Tournament, Game, Showing, TournamentResult, Prize
 
 from django.contrib import admin
 
-admin.site.register(Job)
+admin.site.register(Group)
 admin.site.register(User)
 admin.site.register(Character)
 admin.site.register(Map)
